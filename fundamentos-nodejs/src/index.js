dd5e0dbfc2c92026729e737af6cc24ffb9711db2
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-12-02 21:25:05 -0300

    criando um hello world

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,6 +1,11 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
 const port = 3333;
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World Ignite!" });
+});
 
 app.listen(port);

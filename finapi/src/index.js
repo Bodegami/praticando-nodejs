c1ad7e848475af3d4f6e56f3625dd3dbb0d9d5fd
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-12-02 23:06:08 -0300

    add Post Controller Account

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,4 +1,32 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  const customerAlrearyExists = customers.some(
+    (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlrearyExists) {
+      return response.status(400).json({ error: "Curtomer already exists!"});
+    }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
